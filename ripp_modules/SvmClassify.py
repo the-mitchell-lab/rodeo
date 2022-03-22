70f19f3e0649c492f436d6c5e080c6012b3e0c8c
Bryce Lorenz Kille <brycekille@gmail.com>
2022-03-22 08:57:54 -0500

    SVM updates

--- ripp_modules/SvmClassify.py
+++ ripp_modules/SvmClassify.py
@@ -105,8 +105,8 @@ class SVMRunner(object):
             self.C_option = 283117
             self.gamma_option = 1E-7
         elif peptide_type == 'sacti':
-            self.C_option = 9765625
-            self.gamma_option = 1E-9
+            self.C_option = 301.4
+            self.gamma_option = 7.8E-5
         elif peptide_type == 'linar':
             self.C_option = 1.17462
             self.gamma_option = 0.001
