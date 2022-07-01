27a96766b3d5d7406c4a601e7d77023efc5fe51d
Bryce Kille <brycekille@gmail.com>
2022-06-30 23:57:48 -0500

    Update rodeo_main.py

--- rodeo_main.py
+++ rodeo_main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.7
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #==============================================================================
 # Copyright (C) 2017 Bryce L. Kille
